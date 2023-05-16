78f8d77ee29d00e484d1eed82c9c85fb00e68f04
dasfef <mealhouse3377@gmail.com>
2023-05-16 17:31:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tabhost262/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tabhost262;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabSpec tabSpecSong = tabHost.newTabSpec("SONG").setIndicator("음악별");
+        tabSpecSong.setContent(R.id.tabSong);
+        tabHost.addTab(tabSpecSong);
+
+        TabSpec tabSpecArtist = tabHost.newTabSpec("ARTIST")
+                .setIndicator("가수별");
+        tabSpecArtist.setContent(R.id.tabArtist);
+        tabHost.addTab(tabSpecArtist);
+
+        TabSpec tabSpecAlbum = tabHost.newTabSpec("ALBUM").setIndicator("앨범별");
+        tabSpecAlbum.setContent(R.id.tabAlbum);
+        tabHost.addTab(tabSpecAlbum);
+
+        tabHost.setCurrentTab(0);
+    }
+}
